86fdaf43182bfbdd0b1feabae545fce00f4b9b2d
Emanuel Tesar <siegrift@google.com>
2019-07-09 15:24:06 +0200

    Use local nextjs

--- next.js/packages/next/server/next.js
+++ next.js/packages/next/server/next.js
@@ -1,5 +1,6 @@
 // This file is used for when users run `require('next')`
 module.exports = options => {
+  console.log('Using custom nextjs')
   if (options.dev) {
     const Server = require('./next-dev-server').default
     return new Server(options)
