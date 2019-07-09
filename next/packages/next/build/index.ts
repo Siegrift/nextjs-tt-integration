5bee9f01e516f00f998d47774ff0d8c43b227b4a
Emanuel Tesar <siegrift@google.com>
2019-07-09 13:33:45 +0200

    Add necessary repos

---
+++ next/packages/next/build/index.ts
@@ -0,0 +1,469 @@
+import chalk from 'chalk'
+import {
+  SERVER_DIRECTORY,
+  SERVERLESS_DIRECTORY,
+  PAGES_MANIFEST,
+  CHUNK_GRAPH_MANIFEST,
+  PHASE_PRODUCTION_BUILD,
+} from 'next-server/constants'
+import loadConfig from 'next-server/next-config'
+import nanoid from 'next/dist/compiled/nanoid/index.js'
+import path from 'path'
+import fs from 'fs'
+import { promisify } from 'util'
+import formatWebpackMessages from '../client/dev/error-overlay/format-webpack-messages'
+import { recursiveDelete } from '../lib/recursive-delete'
+import { verifyTypeScriptSetup } from '../lib/verifyTypeScriptSetup'
+import { CompilerResult, runCompiler } from './compiler'
+import { createEntrypoints, createPagesMapping } from './entries'
+import { FlyingShuttle } from './flying-shuttle'
+import { generateBuildId } from './generate-build-id'
+import { isWriteable } from './is-writeable'
+import {
+  collectPages,
+  getCacheIdentifier,
+  getFileForPage,
+  getPageSizeInKb,
+  getSpecifiedPages,
+  printTreeView,
+  PageInfo,
+  isPageStatic,
+  hasCustomAppGetInitialProps,
+} from './utils'
+import getBaseWebpackConfig from './webpack-config'
+import {
+  exportManifest,
+  getPageChunks,
+} from './webpack/plugins/chunk-graph-plugin'
+import { writeBuildId } from './write-build-id'
+import { recursiveReadDir } from '../lib/recursive-readdir'
+import mkdirpOrig from 'mkdirp'
+import workerFarm from 'worker-farm'
+import { Sema } from 'async-sema'
+
+const fsUnlink = promisify(fs.unlink)
+const fsRmdir = promisify(fs.rmdir)
+const fsMove = promisify(fs.rename)
+const fsReadFile = promisify(fs.readFile)
+const fsWriteFile = promisify(fs.writeFile)
+const mkdirp = promisify(mkdirpOrig)
+
+const staticCheckWorker = require.resolve('./static-checker')
+
+export default async function build(dir: string, conf = null): Promise<void> {
+  if (!(await isWriteable(dir))) {
+    throw new Error(
+      '> Build directory is not writeable. https://err.sh/zeit/next.js/build-dir-not-writeable'
+    )
+  }
+
+  await verifyTypeScriptSetup(dir)
+
+  console.log('Creating an optimized production build ...')
+  console.log()
+
+  const config = loadConfig(PHASE_PRODUCTION_BUILD, dir, conf)
+  const { target } = config
+  const buildId = await generateBuildId(config.generateBuildId, nanoid)
+  const distDir = path.join(dir, config.distDir)
+  const pagesDir = path.join(dir, 'pages')
+
+  const isFlyingShuttle = Boolean(
+    config.experimental.flyingShuttle &&
+      !process.env.__NEXT_BUILDER_EXPERIMENTAL_PAGE
+  )
+  const selectivePageBuilding = Boolean(
+    isFlyingShuttle || process.env.__NEXT_BUILDER_EXPERIMENTAL_PAGE
+  )
+
+  if (selectivePageBuilding && target !== 'serverless') {
+    throw new Error(
+      `Cannot use ${
+        isFlyingShuttle ? 'flying shuttle' : '`now dev`'
+      } without the serverless target.`
+    )
+  }
+
+  const selectivePageBuildingCacheIdentifier = selectivePageBuilding
+    ? await getCacheIdentifier({
+        pagesDirectory: pagesDir,
+        env: config.env || {},
+      })
+    : 'noop'
+
+  let flyingShuttle: FlyingShuttle | undefined
+  if (isFlyingShuttle) {
+    console.log(chalk.magenta('Building with Flying Shuttle enabled ...'))
+    console.log()
+
+    await recursiveDelete(distDir, /^(?!cache(?:[\/\\]|$)).*$/)
+    await recursiveDelete(path.join(distDir, 'cache', 'next-minifier'))
+    await recursiveDelete(path.join(distDir, 'cache', 'next-babel-loader'))
+
+    flyingShuttle = new FlyingShuttle({
+      buildId,
+      pagesDirectory: pagesDir,
+      distDirectory: distDir,
+      cacheIdentifier: selectivePageBuildingCacheIdentifier,
+    })
+  }
+
+  let pagePaths: string[]
+  if (process.env.__NEXT_BUILDER_EXPERIMENTAL_PAGE) {
+    pagePaths = await getSpecifiedPages(
+      dir,
+      process.env.__NEXT_BUILDER_EXPERIMENTAL_PAGE!,
+      config.pageExtensions
+    )
+  } else {
+    pagePaths = await collectPages(pagesDir, config.pageExtensions)
+  }
+  // needed for static exporting since we want to replace with HTML
+  // files even when flying shuttle doesn't rebuild the files
+  const allPagePaths = [...pagePaths]
+  const allStaticPages = new Set<string>()
+  let allPageInfos = new Map<string, PageInfo>()
+
+  if (flyingShuttle && (await flyingShuttle.hasShuttle())) {
+    allPageInfos = await flyingShuttle.getPageInfos()
+    const _unchangedPages = new Set(await flyingShuttle.getUnchangedPages())
+    for (const unchangedPage of _unchangedPages) {
+      const info = allPageInfos.get(unchangedPage) || ({} as PageInfo)
+      if (info.static) allStaticPages.add(unchangedPage)
+
+      const recalled = await flyingShuttle.restorePage(unchangedPage, info)
+      if (recalled) {
+        continue
+      }
+      _unchangedPages.delete(unchangedPage)
+    }
+
+    const unchangedPages = (await Promise.all(
+      [..._unchangedPages].map(async page => {
+        if (
+          page.endsWith('.amp') &&
+          (allPageInfos.get(page.split('.amp')[0]) || ({} as PageInfo)).isAmp
+        ) {
+          return ''
+        }
+        const file = await getFileForPage({
+          page,
+          pagesDirectory: pagesDir,
+          pageExtensions: config.pageExtensions,
+        })
+        if (file) {
+          return file
+        }
+
+        return Promise.reject(
+          new Error(
+            `Failed to locate page file: ${page}. ` +
+              `Did pageExtensions change? We can't recover from this yet.`
+          )
+        )
+      })
+    )).filter(Boolean)
+
+    const pageSet = new Set(pagePaths)
+    for (const unchangedPage of unchangedPages) {
+      pageSet.delete(unchangedPage)
+    }
+    pagePaths = [...pageSet]
+  }
+
+  const allMappedPages = createPagesMapping(allPagePaths, config.pageExtensions)
+  const mappedPages = createPagesMapping(pagePaths, config.pageExtensions)
+  const entrypoints = createEntrypoints(
+    mappedPages,
+    target,
+    buildId,
+    /* dynamicBuildId */ selectivePageBuilding,
+    config
+  )
+  const configs = await Promise.all([
+    getBaseWebpackConfig(dir, {
+      buildId,
+      isServer: false,
+      config,
+      target,
+      entrypoints: entrypoints.client,
+      selectivePageBuilding,
+    }),
+    getBaseWebpackConfig(dir, {
+      buildId,
+      isServer: true,
+      config,
+      target,
+      entrypoints: entrypoints.server,
+      selectivePageBuilding,
+    }),
+  ])
+
+  let result: CompilerResult = { warnings: [], errors: [] }
+  if (target === 'serverless') {
+    if (config.publicRuntimeConfig)
+      throw new Error(
+        'Cannot use publicRuntimeConfig with target=serverless https://err.sh/zeit/next.js/serverless-publicRuntimeConfig'
+      )
+
+    const clientResult = await runCompiler(configs[0])
+    // Fail build if clientResult contains errors
+    if (clientResult.errors.length > 0) {
+      result = {
+        warnings: [...clientResult.warnings],
+        errors: [...clientResult.errors],
+      }
+    } else {
+      const serverResult = await runCompiler(configs[1])
+      result = {
+        warnings: [...clientResult.warnings, ...serverResult.warnings],
+        errors: [...clientResult.errors, ...serverResult.errors],
+      }
+    }
+  } else {
+    result = await runCompiler(configs)
+  }
+
+  result = formatWebpackMessages(result)
+
+  if (isFlyingShuttle) {
+    console.log()
+
+    exportManifest({
+      dir: dir,
+      fileName: path.join(distDir, CHUNK_GRAPH_MANIFEST),
+      selectivePageBuildingCacheIdentifier,
+    })
+  }
+
+  if (result.errors.length > 0) {
+    // Only keep the first error. Others are often indicative
+    // of the same problem, but confuse the reader with noise.
+    if (result.errors.length > 1) {
+      result.errors.length = 1
+    }
+    const error = result.errors.join('\n\n')
+
+    console.error(chalk.red('Failed to compile.\n'))
+    console.error(error)
+    console.error()
+
+    if (error.indexOf('private-next-pages') > -1) {
+      throw new Error(
+        '> webpack config.resolve.alias was incorrectly overriden. https://err.sh/zeit/next.js/invalid-resolve-alias'
+      )
+    }
+    throw new Error('> Build failed because of webpack errors')
+  } else if (result.warnings.length > 0) {
+    console.warn(chalk.yellow('Compiled with warnings.\n'))
+    console.warn(result.warnings.join('\n\n'))
+    console.warn()
+  } else {
+    console.log(chalk.green('Compiled successfully.\n'))
+  }
+
+  const distPath = path.join(dir, config.distDir)
+  const pageKeys = Object.keys(mappedPages)
+  const manifestPath = path.join(
+    distDir,
+    target === 'serverless' ? SERVERLESS_DIRECTORY : SERVER_DIRECTORY,
+    PAGES_MANIFEST
+  )
+
+  const staticPages = new Set<string>()
+  const invalidPages = new Set<string>()
+  const pageInfos = new Map<string, PageInfo>()
+  const pagesManifest = JSON.parse(await fsReadFile(manifestPath, 'utf8'))
+  let customAppGetInitialProps: boolean | undefined
+
+  process.env.NEXT_PHASE = PHASE_PRODUCTION_BUILD
+
+  const staticCheckSema = new Sema(config.experimental.cpus, {
+    capacity: pageKeys.length,
+  })
+  const staticCheckWorkers = workerFarm(
+    {
+      maxConcurrentWorkers: config.experimental.cpus,
+    },
+    staticCheckWorker,
+    ['default']
+  )
+
+  await Promise.all(
+    pageKeys.map(async page => {
+      const chunks = getPageChunks(page)
+
+      const actualPage = page === '/' ? '/index' : page
+      const size = await getPageSizeInKb(actualPage, distPath, buildId)
+      const bundleRelative = path.join(
+        target === 'serverless' ? 'pages' : `static/${buildId}/pages`,
+        actualPage + '.js'
+      )
+      const serverBundle = path.join(
+        distPath,
+        target === 'serverless' ? SERVERLESS_DIRECTORY : SERVER_DIRECTORY,
+        bundleRelative
+      )
+
+      let isStatic = false
+
+      pagesManifest[page] = bundleRelative.replace(/\\/g, '/')
+
+      const runtimeEnvConfig = {
+        publicRuntimeConfig: config.publicRuntimeConfig,
+        serverRuntimeConfig: config.serverRuntimeConfig,
+      }
+      const nonReservedPage = !page.match(/^\/(_app|_error|_document|api)/)
+
+      if (nonReservedPage && customAppGetInitialProps === undefined) {
+        customAppGetInitialProps = hasCustomAppGetInitialProps(
+          target === 'serverless'
+            ? serverBundle
+            : path.join(
+                distPath,
+                SERVER_DIRECTORY,
+                `/static/${buildId}/pages/_app.js`
+              ),
+          runtimeEnvConfig
+        )
+
+        if (customAppGetInitialProps) {
+          console.warn(
+            chalk.bold.yellow(`Warning: `) +
+              chalk.yellow(
+                `You have opted-out of Automatic Prerendering due to \`getInitialProps\` in \`pages/_app\`.`
+              )
+          )
+          console.warn(
+            'Read more: https://err.sh/next.js/opt-out-automatic-prerendering\n'
+          )
+        }
+      }
+
+      if (nonReservedPage) {
+        try {
+          await staticCheckSema.acquire()
+          const result: any = await new Promise((resolve, reject) => {
+            staticCheckWorkers.default(
+              { serverBundle, runtimeEnvConfig },
+              (error: Error | null, result: any) => {
+                if (error) return reject(error)
+                resolve(result || {})
+              }
+            )
+          })
+          staticCheckSema.release()
+
+          if (
+            (result.static && customAppGetInitialProps === false) ||
+            result.prerender
+          ) {
+            staticPages.add(page)
+            isStatic = true
+          }
+        } catch (err) {
+          if (err.message !== 'INVALID_DEFAULT_EXPORT') throw err
+          invalidPages.add(page)
+          staticCheckSema.release()
+        }
+      }
+
+      pageInfos.set(page, { size, chunks, serverBundle, static: isStatic })
+    })
+  )
+
+  workerFarm.end(staticCheckWorkers)
+
+  if (invalidPages.size > 0) {
+    throw new Error(
+      `autoExport failed: found page${
+        invalidPages.size === 1 ? '' : 's'
+      } without React Component as default export\n${[...invalidPages]
+        .map(pg => `pages${pg}`)
+        .join(
+          '\n'
+        )}\n\nSee https://err.sh/zeit/next.js/page-without-valid-component for more info.\n`
+    )
+  }
+
+  if (Array.isArray(configs[0].plugins)) {
+    configs[0].plugins.some((plugin: any) => {
+      if (!plugin.ampPages) {
+        return false
+      }
+
+      plugin.ampPages.forEach((pg: any) => {
+        pageInfos.get(pg)!.isAmp = true
+      })
+      return true
+    })
+  }
+
+  await writeBuildId(distDir, buildId, selectivePageBuilding)
+
+  if (staticPages.size > 0) {
+    const exportApp = require('../export').default
+    const exportOptions = {
+      silent: true,
+      buildExport: true,
+      pages: Array.from(staticPages),
+      outdir: path.join(distDir, 'export'),
+    }
+    const exportConfig = {
+      ...config,
+      exportPathMap: (defaultMap: any) => defaultMap,
+      exportTrailingSlash: false,
+    }
+    await exportApp(dir, exportOptions, exportConfig)
+    const toMove = await recursiveReadDir(exportOptions.outdir, /.*\.html$/)
+
+    let serverDir: string = ''
+    // remove server bundles that were exported
+    for (const page of staticPages) {
+      const { serverBundle } = pageInfos.get(page)!
+      if (!serverDir) {
+        serverDir = path.join(
+          serverBundle.split(/(\/|\\)pages/).shift()!,
+          'pages'
+        )
+      }
+      await fsUnlink(serverBundle)
+    }
+
+    for (const file of toMove) {
+      const orig = path.join(exportOptions.outdir, file)
+      const dest = path.join(serverDir, file)
+      const relativeDest = (target === 'serverless'
+        ? path.join('pages', file)
+        : path.join('static', buildId, 'pages', file)
+      ).replace(/\\/g, '/')
+
+      let page = file.split('.html')[0].replace(/\\/g, '/')
+      pagesManifest[page] = relativeDest
+      page = page === '/index' ? '/' : page
+      pagesManifest[page] = relativeDest
+      staticPages.add(page)
+      await mkdirp(path.dirname(dest))
+      await fsMove(orig, dest)
+    }
+    // remove temporary export folder
+    await recursiveDelete(exportOptions.outdir)
+    await fsRmdir(exportOptions.outdir)
+    await fsWriteFile(manifestPath, JSON.stringify(pagesManifest), 'utf8')
+  }
+  staticPages.forEach(pg => allStaticPages.add(pg))
+  pageInfos.forEach((info: PageInfo, key: string) => {
+    allPageInfos.set(key, info)
+  })
+
+  if (flyingShuttle) {
+    await flyingShuttle.mergePagesManifest()
+    await flyingShuttle.save(allStaticPages, pageInfos)
+  }
+
+  printTreeView(
+    Object.keys(allMappedPages),
+    allPageInfos,
+    target === 'serverless'
+  )
+}
---
+++ next/packages/next/client/index.js
@@ -0,0 +1,298 @@
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import HeadManager from './head-manager'
+import { createRouter, makePublicRouterInstance } from 'next/router'
+import mitt from 'next-server/dist/lib/mitt'
+import { loadGetInitialProps, getURL } from 'next-server/dist/lib/utils'
+import PageLoader from './page-loader'
+import * as envConfig from 'next-server/config'
+import Loadable from 'next-server/dist/lib/loadable'
+import { HeadManagerContext } from 'next-server/dist/lib/head-manager-context'
+import { DataManagerContext } from 'next-server/dist/lib/data-manager-context'
+import { RouterContext } from 'next-server/dist/lib/router-context'
+import { DataManager } from 'next-server/dist/lib/data-manager'
+import { parse as parseQs, stringify as stringifyQs } from 'querystring'
+import { isDynamicRoute } from 'next-server/dist/lib/router/utils/is-dynamic'
+
+// Polyfill Promise globally
+// This is needed because Webpack's dynamic loading(common chunks) code
+// depends on Promise.
+// So, we need to polyfill it.
+// See: https://webpack.js.org/guides/code-splitting/#dynamic-imports
+if (!window.Promise) {
+  window.Promise = Promise
+}
+
+const data = JSON.parse(document.getElementById('__NEXT_DATA__').textContent)
+window.__NEXT_DATA__ = data
+
+const {
+  props,
+  err,
+  page,
+  query,
+  buildId,
+  dynamicBuildId,
+  assetPrefix,
+  runtimeConfig,
+  dynamicIds
+} = data
+
+const d = JSON.parse(window.__NEXT_DATA__.dataManager)
+export const dataManager = new DataManager(d)
+
+const prefix = assetPrefix || ''
+
+// With dynamic assetPrefix it's no longer possible to set assetPrefix at the build time
+// So, this is how we do it in the client side at runtime
+__webpack_public_path__ = `${prefix}/_next/` //eslint-disable-line
+// Initialize next/config with the environment configuration
+envConfig.setConfig({
+  serverRuntimeConfig: {},
+  publicRuntimeConfig: runtimeConfig
+})
+
+const asPath = getURL()
+
+const pageLoader = new PageLoader(buildId, prefix)
+const register = ([r, f]) => pageLoader.registerPage(r, f)
+if (window.__NEXT_P) {
+  window.__NEXT_P.map(register)
+}
+window.__NEXT_P = []
+window.__NEXT_P.push = register
+
+const headManager = new HeadManager()
+const appElement = document.getElementById('__next')
+
+let lastAppProps
+let webpackHMR
+export let router
+export let ErrorComponent
+let Component
+let App
+
+class Container extends React.Component {
+  componentDidCatch (err, info) {
+    this.props.fn(err, info)
+  }
+
+  componentDidMount () {
+    this.scrollToHash()
+
+    // If page was exported and has a querystring
+    // If it's a dynamic route or has a querystring
+    if (
+      data.nextExport &&
+      (isDynamicRoute(router.pathname) || window.location.search)
+    ) {
+      // update query on mount for exported pages
+      router.replace(
+        router.pathname +
+          '?' +
+          stringifyQs({
+            ...router.query,
+            ...parseQs(window.location.search.substr(1))
+          }),
+        asPath
+      )
+    }
+  }
+
+  componentDidUpdate () {
+    this.scrollToHash()
+  }
+
+  scrollToHash () {
+    let { hash } = window.location
+    hash = hash && hash.substring(1)
+    if (!hash) return
+
+    const el = document.getElementById(hash)
+    if (!el) return
+
+    // If we call scrollIntoView() in here without a setTimeout
+    // it won't scroll properly.
+    setTimeout(() => el.scrollIntoView(), 0)
+  }
+
+  render () {
+    return this.props.children
+  }
+}
+
+export const emitter = mitt()
+
+export default async ({ webpackHMR: passedWebpackHMR } = {}) => {
+  // This makes sure this specific lines are removed in production
+  if (process.env.NODE_ENV === 'development') {
+    webpackHMR = passedWebpackHMR
+  }
+  App = await pageLoader.loadPage('/_app')
+
+  let initialErr = err
+
+  try {
+    Component = await pageLoader.loadPage(page)
+
+    if (process.env.NODE_ENV !== 'production') {
+      const { isValidElementType } = require('react-is')
+      if (!isValidElementType(Component)) {
+        throw new Error(
+          `The default export is not a React Component in page: "${page}"`
+        )
+      }
+    }
+  } catch (error) {
+    // This catches errors like throwing in the top level of a module
+    initialErr = error
+  }
+
+  await Loadable.preloadReady(dynamicIds || [])
+
+  if (dynamicBuildId === true) {
+    pageLoader.onDynamicBuildId()
+  }
+
+  router = createRouter(page, query, asPath, {
+    initialProps: props,
+    pageLoader,
+    App,
+    Component,
+    err: initialErr,
+    subscription: ({ Component, props, err }, App) => {
+      render({ App, Component, props, err, emitter })
+    }
+  })
+
+  render({ App, Component, props, err: initialErr, emitter })
+
+  return emitter
+}
+
+export async function render (props) {
+  if (props.err) {
+    await renderError(props)
+    return
+  }
+
+  try {
+    await doRender(props)
+  } catch (err) {
+    await renderError({ ...props, err })
+  }
+}
+
+// This method handles all runtime and debug errors.
+// 404 and 500 errors are special kind of errors
+// and they are still handle via the main render method.
+export async function renderError (props) {
+  const { App, err } = props
+
+  if (process.env.NODE_ENV !== 'production') {
+    return webpackHMR.reportRuntimeError(webpackHMR.prepareError(err))
+  }
+
+  // Make sure we log the error to the console, otherwise users can't track down issues.
+  console.error(err)
+
+  ErrorComponent = await pageLoader.loadPage('/_error')
+
+  // In production we do a normal render with the `ErrorComponent` as component.
+  // If we've gotten here upon initial render, we can use the props from the server.
+  // Otherwise, we need to call `getInitialProps` on `App` before mounting.
+  const initProps = props.props
+    ? props.props
+    : await loadGetInitialProps(App, {
+      Component: ErrorComponent,
+      router,
+      ctx: { err, pathname: page, query, asPath }
+    })
+
+  await doRender({ ...props, err, Component: ErrorComponent, props: initProps })
+}
+
+// If hydrate does not exist, eg in preact.
+let isInitialRender = typeof ReactDOM.hydrate === 'function'
+function renderReactElement (reactEl, domEl) {
+  // The check for `.hydrate` is there to support React alternatives like preact
+  if (isInitialRender) {
+    ReactDOM.hydrate(reactEl, domEl)
+    isInitialRender = false
+  } else {
+    ReactDOM.render(reactEl, domEl)
+  }
+}
+
+function AppContainer ({ children }) {
+  return (
+    <Container
+      fn={error =>
+        renderError({ App, err: error }).catch(err =>
+          console.error('Error rendering page: ', err)
+        )
+      }
+    >
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterContext.Provider value={makePublicRouterInstance(router)}>
+          <DataManagerContext.Provider value={dataManager}>
+            <HeadManagerContext.Provider value={headManager.updateHead}>
+              {children}
+            </HeadManagerContext.Provider>
+          </DataManagerContext.Provider>
+        </RouterContext.Provider>
+      </Suspense>
+    </Container>
+  )
+}
+
+async function doRender ({ App, Component, props, err }) {
+  // Usual getInitialProps fetching is handled in next/router
+  // this is for when ErrorComponent gets replaced by Component by HMR
+  if (
+    !props &&
+    Component &&
+    Component !== ErrorComponent &&
+    lastAppProps.Component === ErrorComponent
+  ) {
+    const { pathname, query, asPath } = router
+    props = await loadGetInitialProps(App, {
+      Component,
+      router,
+      ctx: { err, pathname, query, asPath }
+    })
+  }
+
+  Component = Component || lastAppProps.Component
+  props = props || lastAppProps.props
+
+  const appProps = { Component, err, router, ...props }
+  // lastAppProps has to be set before ReactDom.render to account for ReactDom throwing an error.
+  lastAppProps = appProps
+
+  emitter.emit('before-reactdom-render', {
+    Component,
+    ErrorComponent,
+    appProps
+  })
+
+  // In development runtime errors are caught by react-error-overlay.
+  if (process.env.NODE_ENV === 'development') {
+    renderReactElement(
+      <AppContainer>
+        <App {...appProps} />
+      </AppContainer>,
+      appElement
+    )
+  } else {
+    // In production we catch runtime errors using componentDidCatch which will trigger renderError.
+    renderReactElement(
+      <AppContainer>
+        <App {...appProps} />
+      </AppContainer>,
+      appElement
+    )
+  }
+
+  emitter.emit('after-reactdom-render', { Component, ErrorComponent, appProps })
+}
